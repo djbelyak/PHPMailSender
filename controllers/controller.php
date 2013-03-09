ee6eb4a7d56236f7308a3b5042cb8ac3a299d784
Иван Белявцев <djbelyak@gmail.com>
2013-03-09 23:12:15 +0400

    Interface restyling

---
+++ controllers/controller.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: djbelyak
+ * Date: 09.03.13
+ * Time: 22:48
+ * To change this template use File | Settings | File Templates.
+ */
+class controller
+{
+    public function controller()
+    {
+        echo "Конструктор, блеать!";
+    }
+
+    public function sendText()
+    {
+
+    }
+
+    public function sendPaper()
+    {
+
+    }
+}
+new controller();
